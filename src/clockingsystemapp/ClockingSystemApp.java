eee3246e8bf5287493bee7e387b5ca4e355edcd4
TechFortune-maker <osegomere222@gmail.com>
2025-03-20 10:22:12 +0200

    First commit

---
+++ src/clockingsystemapp/ClockingSystemApp.java
@@ -0,0 +1,16 @@
+
+package clockingsystemapp;
+
+import za.ac.tut.ui.LoginFrame;
+
+
+public class ClockingSystemApp {
+
+    
+    public static void main(String[] args) {
+       
+        new LoginFrame();
+        
+    }
+    
+}
